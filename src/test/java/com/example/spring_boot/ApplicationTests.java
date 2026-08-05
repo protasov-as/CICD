3bddad1d98d283f4d30aebd595b032da64813453
protasov-as <110101662+protasov-as@users.noreply.github.com>
2026-08-05 10:29:11 +0300

    Update ApplicationTests.java

--- src/test/java/com/example/spring_boot/ApplicationTests.java
+++ src/test/java/com/example/spring_boot/ApplicationTests.java
@@ -44,6 +44,6 @@ class ApplicationTests {
 	@DisplayName("Проверка Mockito")
 	public void basicMockTest() {
 		Mockito.when(calc.sum(77, 77)).thenReturn(7777);
-		assertEquals(777712, calc.sum(77, 77));
+		assertEquals(777713, calc.sum(77, 77));
 	}
 }
